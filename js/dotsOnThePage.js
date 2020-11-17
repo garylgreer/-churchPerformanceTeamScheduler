3f64ead7143a0306dc22303fd4c88ef2ce67fc87
Gary L Greer <garylgreer@att.net>
2020-11-17 17:23:43 -0500

    Array definitions

---
+++ js/dotsOnThePage.js
@@ -0,0 +1,6 @@
+var dotsOnThePage = new Array
+dotsOnThePage[0] = 'Alive & Breathing';
+dotsOnThePage[1] = 'Give Me Faith';
+dotsOnThePage[2] = 'Holy Water';
+dotsOnThePage[3] = 'Live Like That';
+dotsOnThePage[4] = 'Who You Say I Am';
---
+++ js/role.js
@@ -0,0 +1,15 @@
+var role = new Array
+role[0] = 'Grand Piano';
+role[1] = 'Electric Guitar';
+role[2] = 'Acoustic Guitar';
+role[3] = 'Bass Guitar';
+role[4] = 'Drums';
+role[5] = 'Keyboard';
+role[6] = 'Lead Vocal';
+role[7] = 'Harmony Vocal';
+role[8] = 'Brass Ensemble';
+role[9] = 'LiveStream';
+role[10] = 'ProPresenter';
+role[11] = 'Lighting';
+role[12] = 'Soundboard';
+role[13] = 'Camera';
